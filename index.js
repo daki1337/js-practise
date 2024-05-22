24c942adf247247d9ff490f9922d72733f728d0c
daki <prokopevnazar4@gmail.com>
2024-05-22 18:49:16 +0300

    Js-Practise

---
+++ index.js
@@ -0,0 +1,96 @@
+console.log(`1 task`);
+console.log();
+
+//  if (confirm('Добрий день, чи хочете Ви розрахувати площу та периметр прямокутника?')) {
+//     const lengthRectangle = prompt ('Введіть довижну прямокутника' );
+//     const widtheRectangle = prompt ('Введіть ширину прямокутника' );
+//     const perimetrRectangle = (Number(lengthRectangle) + Number(widtheRectangle));
+//     const areaRectangle = ( Number(lengthRectangle) * Number(widtheRectangle));
+//     const resultPerimetr = alert('Ваш периметр - ' + perimetrRectangle);
+//     const resultArea = alert ('Ваша площа - ' + areaRectangle);
+// } else {
+//     if (confirm ('Ви впевнені?')) {
+//         alert('Добре, до побачення!')
+//     } else {
+//         const lengthRectangle = prompt ('Введіть довижну прямокутника' );
+//     const widtheRectangle = prompt ('Введіть ширину прямокутника' );
+//     const perimetrRectangle = (Number(lengthRectangle) + Number(widtheRectangle));
+//     const areaRectangle = ( Number(lengthRectangle) * Number(widtheRectangle));
+//     const resultPerimetr = alert('Ваш периметр - ' + perimetrRectangle);
+//     const resultArea = alert ('Ваша площа - ' + areaRectangle);
+//     }
+// }
+console.groupEnd();
+// console.log(Math.PI);
+// if (confirm('Добрий день, чи хочете Ви розрахувати площу та довжину кола маючи тільки радіус?')){
+//     const constRadius = prompt ('Введіть ваш радіус');
+    
+//     const areaO = (Math.PI) * (constRadius * constRadius);
+
+//     alert('Площа кола = ' + areaO );
+//     alert('Довжина кола = ' + (2) *  (Math.PI) * constRadius);
+// }
+// else {
+// alert('До побачення!')
+
+// }
+console.groupEnd();
+
+// if (confirm('Добрий день, чи хочете Ви розрахувати об\'єм куба, знаючи лише довжину його ребра?')) {
+//     const lengthOfСubeEdge = prompt ('Введіть довжину ребра цього куба');
+//     alert('Об\'єм цього кута = ' + lengthOfСubeEdge * lengthOfСubeEdge * lengthOfСubeEdge);
+// } else {
+//     alert('До побачення!')
+// }
+
+console.groupEnd();
+// console.log(Math.PI);
+// if (confirm('Добрий день, чи хочете Ви розрахувати об\'єм циліндру, знаючи лише його радіус основи та висоту? ')) {
+//     const radiusOfCylinder  = prompt ('Введіть радіус Вашого циліндру');
+//     const heightOfCylindre = prompt ('Введіть висоту Вашого циліндру');
+//     const resultOfCylinder = Math.PI * radiusOfCylinder  * heightOfCylindre;
+//     alert("Об'єм Вашого циліндру = " + resultOfCylinder);
+// } else {
+//     alert('Добре, до побачення!');
+// }
+console.groupEnd();
+// if (confirm('Добрий день, чи хочете Ви привітатися англійською мовою? Якщо Ви відмовите я привітаюсь українською!')) {
+//     alert('Hello!')
+// } else {
+//     alert(`Добрий день!`)
+// }
+console.groupEnd();
+// if (confirm('Добрий день чи хочете Ви зробити розрахунок?')) {
+//     const price = prompt ('Введіть суму рахунку');
+//     const percentages = prompt ('Введіть відсоток чайових');
+//     const allPrice = price + price * percentages;
+//     alert('Сума рахунку разом х чайовими - ' + allPrice)
+// } else {
+//     alert('До побачення!');
+// }
+console.groupEnd();
+// // const MoneyForHobby7days = prompt('Введіть, будь ласка суму яку Ви витрачаєте на своє хобі за тиждень')
+// // const MoneyForHobby30days = MoneyForHobby7days * 4.28571428571;
+// // if (confirm('Чи хочете Ви, щоби я підрахував скільки Ви витрачаєте на своє хобі за місяць?')) {
+// //     alert('Ви витрачаєте на своє хобі за місяць - ' + MoneyForHobby30days)
+// // } else {
+// //     alert('Добре, до побачення!');
+
+// }
+console.groupEnd();
+const MoneyForWiFi30days = prompt('Введіть, будь ласка суму яку Ви витрачаєте грошей на інтернет за місяць')
+const MoneyForWiFi365days = MoneyForWiFi365days * 12.1666666667;
+if (confirm('Чи хочете Ви, щоби я підрахував скільки Ви витрачаєте на свій інтернет за рік?')) {
+    alert('Ви витрачаєте на свій інтернет за рік - ' + MoneyForWiFi365days)
+} else {
+    alert('Добре, до побачення!');
+
+}
+
+
+
+
+
+
+ 
+
