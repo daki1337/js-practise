4b27395b8fa246c9e66740791cab9badf398c8c2
daki <prokopevnazar4@gmail.com>
2024-05-27 17:42:13 +0300

    Update + add something

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const number = 49;
-const remainder = number%7;
-if (remainder === 0) {
-    console.log('Число кратне' + remainder);
-} else {
-    console.log('Число не кратне' + remainder);
-}
+// const number = 49;
+// const remainder = number%7;
+// if (remainder === 0) {
+//     console.log('Число кратне' + remainder);
+// } else {
+//     console.log('Число не кратне' + remainder);
+// }
+
+//false
+//underfined
+console.log(Boolean(undefined));
+//null
+console.log(Boolean(null));
+// ''   ""   ``
+console.log(Boolean(''));
+//0 +0 -0;
+console.log(Boolean(0));(false)
+//NaN - помилка обчислення
+console.log(Boolean(NaN)); //false
+
+
+// ! NOT інверсія
+
+console.log(!true); //false
+console.log(!false); //true
+
+const str = '';
+console.log(!str); //true !false
+
+console.log(!undefined); //false -> true
 
