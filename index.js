f42f8cc45369a9aea53791be92f613e8c83b2698
daki <prokopevnazar4@gmail.com>
2024-05-27 19:00:01 +0300

    Update again

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-// const number = 49;
-// const remainder = number%7;
-// if (remainder === 0) {
-//     console.log('Число кратне' + remainder);
+//false
+//undefined
+//null
+//''  ""  ``
+//0 +0 -0
+//Nan - помилка обчислення
+
+// const userAgeInput = prompt ('Введіть свій вік');
+// const startSchoolAge = 6;
+// const finishSchoolAge = 17;
+
+// const userAge = Number(userAgeInput); //11
+
+// if (userAge>=6 && userAge <= 17) {
+//     alert('school')
 // } else {
-//     console.log('Число не кратне' + remainder);
+//     alert('Not school')
 // }
 
-//false
-//underfined
-console.log(Boolean(undefined));
-//null
-console.log(Boolean(null));
-// ''   ""   ``
-console.log(Boolean(''));
-//0 +0 -0;
-console.log(Boolean(0));(false)
-//NaN - помилка обчислення
-console.log(Boolean(NaN)); //false
+// людина вводить число і якщо воно кратне 3 і 7 одночасно, ви виводите win! інакше - loss
+// const count = prompt('Введіть число');
+// const remainder1 = count%7 ;
+// const remainder2 = count%3 ;
 
+ 
 
-// ! NOT інверсія
+// if (remainder1 === 0 && remainder2 === 0) {
+//     alert('WIN!')
+// } else {
+//     alert('LOSE!')
+// }
 
-console.log(!true); //false
-console.log(!false); //true
+const userInput = prompt('Enter number');
+console.log(userInput);
+const userNumber = Number(userInput);
+console.log(userNumber);
 
-const str = '';
-console.log(!str); //true !false
+//перша помилка - користувач натиснув відміну
+const error1 = userInput === null;
+//друга помилка - користувач надіслав порожній рядок
+const error2 = userInput === '';
+//третя помилка - користувач заміть цифер ввів літери
+const error3 = userInput === Number.isNaN(userInput);
 
-console.log(!undefined); //false -> true
-
+if (error1 || error2 || error3){
+    alert('Error.\n'+ userInput + ' - not a number!!!')
+}else{
+    if (userNumber>= 0 && userNumber <= 23) {
+        alert('thanks');
+    } else {
+        alert('Error.\n' + userInput + ' - not a hours!!!'
+        )
+    }
+}
