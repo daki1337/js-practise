2224a460d23d713a745d06f07884b1343ca6aa44
daki <prokopevnazar4@gmail.com>
2024-05-29 19:03:39 +0300

    Ternary operator

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
-//false
-//undefined
-//null
-//''  ""  ``
-//0 +0 -0
-//Nan - помилка обчислення
+// const inputUserAge = prompt ('Введіть Ваш вік')
+// const priceTicket = 100;
+// const userAge = Number(inputUserAge);
 
-// const userAgeInput = prompt ('Введіть свій вік');
-// const startSchoolAge = 6;
-// const finishSchoolAge = 17;
+// const sale = (userAge < 18 || userAge > 65 ) ? 10 : 0;
+// const saleCheck = priceTicket - (priceTicket*sale/100);
+// alert ('До сплати - ' + saleCheck)
 
-// const userAge = Number(userAgeInput); //11
+// const userInput = prompt ('Введіть число');
+// const number = Number(userInput);
+// const moduleOfNumber =  (number1 >= 0) ? number1 : -number1;
+// alert(moduleOfNumber);
+const userInput1 = prompt ('Enter side 1')
+const userInput2 = prompt ('Enter side 2')
 
-// if (userAge>=6 && userAge <= 17) {
-//     alert('school')
-// } else {
-//     alert('Not school')
-// }
+const titleFigure = (userInput1 === userInput2) ? 'квадрат' : 'прямокутник';
 
-// людина вводить число і якщо воно кратне 3 і 7 одночасно, ви виводите win! інакше - loss
-// const count = prompt('Введіть число');
-// const remainder1 = count%7 ;
-// const remainder2 = count%3 ;
-
- 
-
-// if (remainder1 === 0 && remainder2 === 0) {
-//     alert('WIN!')
-// } else {
-//     alert('LOSE!')
-// }
-
-const userInput = prompt('Enter number');
-console.log(userInput);
-const userNumber = Number(userInput);
-console.log(userNumber);
-
-//перша помилка - користувач натиснув відміну
-const error1 = userInput === null;
-//друга помилка - користувач надіслав порожній рядок
-const error2 = userInput === '';
-//третя помилка - користувач заміть цифер ввів літери
-const error3 = userInput === Number.isNaN(userInput);
-
-if (error1 || error2 || error3){
-    alert('Error.\n'+ userInput + ' - not a number!!!')
-}else{
-    if (userNumber>= 0 && userNumber <= 23) {
-        alert('thanks');
-    } else {
-        alert('Error.\n' + userInput + ' - not a hours!!!'
-        )
-    }
-}
+alert(titleFigure);
