e7f107b7db28705f884e315268c6c33d91cb55d7
daki <prokopevnazar4@gmail.com>
2024-05-31 18:16:34 +0300

    Add example switch and ternary

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-// const inputUserAge = prompt ('Введіть Ваш вік')
-// const priceTicket = 100;
-// const userAge = Number(inputUserAge);
+// const message = `
+// Введи мову якою хочешь спілкуватися:
+// українська - ua;
+// англійскька - en;
+// польска - pl;
+// іспаньска - es;
+// французька - fr;`; 
+// const lang = prompt(message);
+// switch (lang) {
+//     case 'ua' || lang==='1' || lang === 'українська':
+//         alert('Привіт!')
+//         break;
+//         case 'en' || lang==='2' || lang === 'англійскька':
+//             alert('Hi!')
+//             break;
+//             case 'pl' || lang==='3' || lang === 'польска':
+//             alert('Сześć')
+//             break;
+//             case 'es' || lang==='4' || lang === 'іспаньска':
+//             alert('Hola!')
+//             break;
+//             case 'fr' || lang==='5' || lang === 'французька':
+//             alert('Bonjour!')
+//             break;
+//     default:
+//         alert('Мова не підтримується')
+//         break;
+// }
 
-// const sale = (userAge < 18 || userAge > 65 ) ? 10 : 0;
-// const saleCheck = priceTicket - (priceTicket*sale/100);
-// alert ('До сплати - ' + saleCheck)
-
-// const userInput = prompt ('Введіть число');
-// const number = Number(userInput);
-// const moduleOfNumber =  (number1 >= 0) ? number1 : -number1;
-// alert(moduleOfNumber);
-const userInput1 = prompt ('Enter side 1')
-const userInput2 = prompt ('Enter side 2')
-
-const titleFigure = (userInput1 === userInput2) ? 'квадрат' : 'прямокутник';
-
-alert(titleFigure);
+// if(lang === 'ua'){
+//     alert('Привіт!')
+// }else if (lang === 'en') { 
+//     alert('Hi')
+// }else if (lang == 'pl' ){
+//     alert('Cześć!')
+// }else if (lang =='es' ){
+//     alert('Hola!')
+// }else if (lang == 'fr' ){
+//     alert('Bonjour')
+// }else{
+//     alert('Мова не підтримується')
+// }
+switch (confirm('Ви хочете покинути сайт?')) {
+    case true:
+        alert('До побачення!');
+        break;
+case false:
+alert('Ми раді, що Ви лишаєтесь з нами!');
+break;
+    default:
+        break;
+}
+confirm('Ви хочете покинути сайт?')
+? alert ('До побачення!')
+:alert ('Ми раді, що Ви лишаєтесь з нами!')
