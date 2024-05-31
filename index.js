5593e9d02cb9b04cbf2db80feff531ca31746510
daki <prokopevnazar4@gmail.com>
2024-05-31 19:02:53 +0300

    Update

--- index.js
+++ index.js
@@ -73,23 +73,23 @@
 //             alert('Такого дня тиждня не існує!')
 //             break;
 //     }
-const userInput1 = prompt('Enter first number')
-const userInput2 = prompt('Enter second number')
+// const userInput1 = prompt('Enter first number')
+// const userInput2 = prompt('Enter second number')
 
-const number1 = Number(userInput1)
-const number2 = Number(userInput2)
+// const number1 = Number(userInput1)
+// const number2 = Number(userInput2)
 
-const operator = prompt('Enter operator: +, -, *, /, % ')
+// const operator = prompt('Enter operator: +, -, *, /, % ')
 
-switch (operator) {
-    case '+':
-        const result = number1 + number2;
-        alert ( number1 + '+' + number2 + '=' + result);
-        break;
+// switch (operator) {
+//     case '+':
+//         const result = number1 + number2;
+//         alert ( number1 + '+' + number2 + '=' + result);
+//         break;
 
-    default:
-        alert('operator not exist');
-        break;
-}
+//     default:
+//         alert('operator not exist');
+//         break;
+// }
 
 
