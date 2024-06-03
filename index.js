64cabfba705c3c1dbcd74889fd5c667742c5496c
daki <prokopevnazar4@gmail.com>
2024-06-03 18:58:38 +0300

    Update

--- index.js
+++ index.js
@@ -91,5 +91,120 @@
 //         alert('operator not exist');
 //         break;
 // }
+// const numberOfOldUser = prompt ('Введіть ваш вік')
+// const oldUser = Number(numberOfOldUser);
+
+// const result = oldUser < 18 ? "Неповнолітній" : "Повнолітній"
+// alert(result);
+// const numberUserInput = prompt ('Введіть ваш вік')
+// const userInput = Number(numberUserInput)
+// if ( 65 >= userInput >= 18  ) {
+//     alert('Ви дороса людина')
+// } else if(18 >= userInput && userInput >= 12 ){
+//     alert('Ви підліток')
+// } else{
+//     alert('Ви літня людина')
+// }
+
+// const numberTimeOfTheDay = prompt ('Введіть, котра зараз година');
+// const timeOfTheDay = Number(numberTimeOfTheDay);
+
+// if ( 11 >= timeOfTheDay && timeOfTheDay >= 5 ) {
+//     alert('Добрий ранок!')
+// } else if ( 18 >= timeOfTheDay && timeOfTheDay>= 11){
+//     alert('Добрий день!')
+// }else if( 24 >= timeOfTheDay && timeOfTheDay>= 22){
+//     alert('На добраніч!')
+// } else if( 5 >= timeOfTheDay && timeOfTheDay>= 0){
+//  alert('На добраніч!')
+// }else if ( 22 >= timeOfTheDay && timeOfTheDay >= 18 ){
+// alert('Добрий вечір!')
+// }else{
+//     alert('Такого часу не існує')
+// }
+
+// const numberHourUserInput = prompt('Введіть, котра зараз година')
+// const hourUserInput = Number(numberHourUserInput);
 
 
+//  const result = 17 > hourUserInput && hourUserInput > 9 ? 
+//  'Працює' : 'Не працює'
+
+// alert('Банк' + result)
+// const userInput = prompt('Введіть номер місяця')
+
+// switch (userInput) {
+//     case '1' :
+//         alert('January')
+//         break;
+//         case '2':
+//             alert('February')
+//         break;
+//         case '3':
+//             alert('March')
+//         break;
+//         case '4':
+//             alert('April')
+//         break;
+//         case  '5':
+//             alert('May')
+//         break;
+//         case  '6':
+//             alert('June')
+//         break;
+//         case  '7':
+//             alert('July')
+//         break;
+//         case  '8':
+//             alert('August')
+//         break;
+//         case  '9':
+//             alert('September')
+//         break;
+//         case  '10':
+//         alert('October')
+//         break;
+//         case  '11':
+//             alert('November')
+//         break;
+//         case  '12':
+//             alert('December')
+//         break;
+
+
+//     default:
+//         alert('Такого місяця не існує')
+//         break;
+// }
+const userInput = prompt('Введіть номер місяця')
+switch (userInput) {
+    // case '1' :
+    //     case  '12':
+    //         case '2':
+    //     alert('Winter')
+    //     break;
+    //     case '3':
+    //         case '4':
+    //             case  '5':
+    //         alert('spring')
+    //     break;
+    //     case  '6':
+    //         case  '7':
+    //             case  '8':
+    //         alert('summer')
+    //     break;
+        
+    //     case  '9':
+    //         case  '10':
+    //             case  '11':
+    //         alert('Autumn')
+    //     break;
+        
+
+
+    // default:
+    //     alert('Такого місяця не існує')
+    //     break; }
+    const side1
+     const side2
+    const side3 
