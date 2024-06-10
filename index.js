248e55256b4e800943b34ccd8d65c8ec40835703
daki <prokopevnazar4@gmail.com>
2024-06-10 19:00:01 +0300

    Update

--- index.js
+++ index.js
@@ -16,3 +16,39 @@
 //     alert('Спробуй ще раз');
 // }
 
+// const start = 15;
+// const end = 25;
+
+// for (let i = start; i < end; i++){
+//     if(i % 4 === 0 && i % 5 === 0){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// const start = 1;
+// const end = 10;
+
+// let summa = 0;
+
+// for ( let i = start; i <= end; i++){
+// if ( i%2 === 0 || i%3 === 0){
+//     continue;
+// }
+// summa +=i
+// }
+// console.log(summa)
+
+// const number = 5;
+
+// for(let i = 1; i <=10; i++){
+//     console.log(`${number}*${i}=${number*i}`);
+// }
+
+// for ( let num1 = 1; num1 <= 10; num1++){
+//     console.group(num1);
+//     for( let num2 = 1; num2 <= 10; num2++){
+//         console.log(`${num1} * ${num2} = ${num1*num2}`);
+//     }
+//     console.groupEnd();
+// }
