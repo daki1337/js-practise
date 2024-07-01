0ce078c8baade2c5a7a46970aa45fab61c40f22b
daki <prokopevnazar4@gmail.com>
2024-07-01 19:00:46 +0300

    Update

--- index.js
+++ index.js
@@ -370,3 +370,55 @@
 
 // alert(colors);
 
+// function declaration
+// function logWord1 (word){
+// console.log(word);
+// }
+// logWord1('test')
+
+//function expression
+// const logWord2 = function (word){
+//     console.log(word);
+// }
+// logWord2 ('new text')
+
+//arrow function
+// const logWord3 = (word) => {
+//     console.log(word);
+
+// }
+
+//arrow function
+// const getPerimetr = (side) => (side*4)
+
+// const site = {
+//     title:'my site',
+//     headers: ['qwe1', 'qwe2','qwe3'],
+//     showHeaders(){
+//         this.headers.forEach(function(elem){
+//             console.log(elem)
+//         })
+//     },
+//     showHeadersAndTitle(){
+//         this.headers.forEach((elem) =>{
+//             console.log(elem,this.title);
+//         })
+//     },
+//     showHeadersAndTitle2(){
+//         this.headers.forEach(function(elem){
+//             console.log(elem, this.title);
+//         })
+//     },
+// }
+
+// site.showHeaders()
+// site.showHeadersAndTitle()
+// site.showHeadersAndTitle2()
+
+//reduce
+
+// const summa = numbers.reduce((accumulator, number)=>accumulator+number);
+// console.log(summa);
+// console.log(numbers);
+
+// const summaOddNumbers = numbers.filter((elem)=>elem%2===1)
